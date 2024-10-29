04dfc2b2e4fc22ea7be46470ffe33b25cdcaff75
Sumit Gusain <sumitgusain5311@gmail.com>
2024-10-29 07:44:28 +0530

    final completed project

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -62,6 +62,14 @@ if(coinData && historicalData){
         <li>Market Cap</li>
         <li>{currency.symbol} {coinData.market_data.market_cap[currency.name].toLocaleString()}</li>
       </ul>
+      <ul>
+        <li>24H Hour high</li>
+        <li>{currency.symbol} {coinData.market_data.high_24h[currency.name].toLocaleString()}</li>
+      </ul>
+      <ul>
+        <li>24H Hour Low</li>
+        <li>{currency.symbol} {coinData.market_data.low_24h[currency.name].toLocaleString()}</li>
+      </ul>
     </div>
 
     </div>
